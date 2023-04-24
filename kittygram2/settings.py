1b1df5552062dd28d32cf7330a4efeac40e71b95
akobelev <akobelev@clink.tech>
2023-04-24 09:23:23 +0400

    Настройка проекта на работу с Postgres

--- kittygram2/settings.py
+++ kittygram2/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'kittygram2.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kittygram',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
